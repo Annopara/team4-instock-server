e43b93fc5a4528e1c03e3ae92aac9555c4f821ab
Ann Opara <oparaannie51@gmail.com>
2024-02-06 08:52:43 -0800

    updates routes and index to include basic routes

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,3 +1,12 @@
 const router = require("express").Router();
 
+router
+  .route("/")
+  .get((_req, res) => {
+    res.status(200).json("Testing inventory route");
+  })
+  .post((_req, res) => {
+    res.status(201).json("");
+  });
+
 module.exports = router;
