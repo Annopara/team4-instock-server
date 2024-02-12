ef1146b62e547fb5a7de8d43ed283494e81c802b
Asia <mohamedasia1@gmail.com>
2024-02-11 21:08:14 -0500

    Get a single Inventory Item

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -1,44 +1,10 @@
-const knex = require("knex")(require("../knexfile"));
-
+const knex = require("knex")(require("../knexfile.js"));
 const index = async (_req, res) => {
   try {
     const data = await knex("inventories");
     res.status(200).json(data);
   } catch (err) {
     res.status(400).send(`Error retrieving data: ${err}`);
-  }
-};
-
-const findOne = async (req, res) => {
-  try {
-    const usersFound = await knex("inventories").where({ id: req.params.id });
-
-    if (usersFound.length === 0) {
-      return res.status(404).json({
-        message: `User with ID ${req.params.id} not found`,
-      });
-    }
-
-    const userData = usersFound[0];
-    res.json(userData);
-  } catch (error) {
-    res.status(500).json({
-      message: `Unable to retrieve user data for user with ID ${req.params.id}`,
-    });
-  }
-};
-
-const posts = async (req, res) => {
-  try {
-    const posts = await knex("inventories")
-      .join("post", "post.user_id", "user.id")
-      .where({ user_id: req.params.id });
-
-    res.json(posts);
-  } catch (error) {
-    res.status(500).json({
-      message: `Unable to retrieve posts for user with ID ${req.params.id}: ${error}`,
-    });
   }
 };
 
@@ -49,11 +15,14 @@ const add = async (req, res) => {
     });
   }
 
+
   try {
     const result = await knex("inventories").insert(req.body);
 
+
     const newUserId = result[0];
     const createdUser = await knex("inventories").where({ id: newUserId });
+
 
     res.status(201).json(createdUser);
   } catch (error) {
@@ -63,11 +32,13 @@ const add = async (req, res) => {
   }
 };
 
+
 const update = async (req, res) => {
   try {
     const rowsUpdated = await knex("inventories")
       .where({ id: req.params.id })
       .update(req.body);
+
 
     if (rowsUpdated === 0) {
       return res.status(404).json({
@@ -75,9 +46,11 @@ const update = async (req, res) => {
       });
     }
 
+
     const updatedUser = await knex("inventories").where({
       id: req.params.id,
     });
+
 
     res.json(updatedUser[0]);
   } catch (error) {
@@ -87,23 +60,39 @@ const update = async (req, res) => {
   }
 };
 
+
 const remove = async (req, res) => {
   try {
-    const rowsDeleted = await knex("inventories")
+    const deleteInventory = await knex("inventories")
       .where({ id: req.params.id })
       .delete();
-
-    if (rowsDeleted === 0) {
+    if (deleteInventory === 0) {
       return res
         .status(404)
-        .json({ message: `User with ID ${req.params.id} not found` });
+        .json({ message: `Inventory with ID ${req.params.id} not found` });
     }
-
     // No Content response
     res.sendStatus(204);
   } catch (error) {
     res.status(500).json({
-      message: `Unable to delete user: ${error}`,
+      message: `Unable to delete Inventory: ${error}`,
+    });
+  }
+};
+
+const findOne = async (req, res) => {
+  try {
+    const usersFound = await knex("inventories").where({ id: req.params.id });
+    if (usersFound.length === 0) {
+      return res.status(404).json({
+        message: `User with ID ${req.params.id} not found`,
+      });
+    }
+    const userData = usersFound[0];
+    res.json(userData);
+  } catch (error) {
+    res.status(500).json({
+      message: `Unable to retrieve user data for user with ID ${req.params.id}`,
     });
   }
 };
@@ -111,8 +100,7 @@ const remove = async (req, res) => {
 module.exports = {
   index,
   findOne,
-  posts,
-  add,
+  add, 
   update,
-  remove,
-};
+  remove
+}
